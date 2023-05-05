e1bd524564a4fd7a29a9bbe990cdc9b287442f52
Nishant Viroja <81224226+Nishantviroja@users.noreply.github.com>
2023-05-05 22:51:50 +0530

    home page height remove

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -9,7 +9,8 @@ function Alert(props) {
         return lower.charAt(0).toUpperCase() + lower.slice(1);
     }
     return (
-        <div style={{height: '50px'}}>
+        <div >
+        {/* <div style={{height: '50px'}}> */}
         {props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
            <strong>{capitalize(props.alert.type)}</strong>: {props.alert.msg} 
         </div>}
