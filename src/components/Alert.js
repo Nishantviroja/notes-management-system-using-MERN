062f950029ba4a07c2f44223ed3af2046a7d8eb7
Nishant Viroja <81224226+Nishantviroja@users.noreply.github.com>
2023-05-05 23:05:47 +0530

    change in footer

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -10,7 +10,7 @@ function Alert(props) {
     }
     return (
         <div >
-        {/* <div style={{height: '50px'}}> */}
+        
         {props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
            <strong>{capitalize(props.alert.type)}</strong>: {props.alert.msg} 
         </div>}
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <footer class="bg-light text-center mt-5 text-lg-start">
+    <footer class="bg-light text-center  text-lg-start">
   
   <div class="text-center p-3 ">
     © 2023 Copyright :<span class="text-dark" > SKYNotE.com || Team PnR <i class="fa-solid fa-heart text-danger"></i> </span>
